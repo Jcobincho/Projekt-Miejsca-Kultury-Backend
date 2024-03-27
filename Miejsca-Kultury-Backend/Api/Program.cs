39a814bf0c1d412bfea3750638bfcd4f38ab443a
SebeK2221 <144722562+SebeK2221@users.noreply.github.com>
2024-03-27 16:37:04 +0100

    Clean architecture + dependency injection
    Clean architecture + dependency injection

---
+++ Miejsca-Kultury-Backend/Api/Program.cs
@@ -0,0 +1,26 @@
+using Application;
+using Infrastructure;
+using Presentation;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddPresentation();
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+    
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
