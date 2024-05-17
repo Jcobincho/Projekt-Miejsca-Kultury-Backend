7b7c55d50b77e9ff371a5214a0d49388c3a539e0
SebeK2221 <144722562+SebeK2221@users.noreply.github.com>
2024-05-17 21:39:25 +0200

    link change 2

--- Miejsca-Kultury-Backend/Application/obj/Debug/net7.0/Application.AssemblyInfo.cs
+++ Miejsca-Kultury-Backend/Application/obj/Debug/net7.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2f5e576f105ed4dcd8a02758d0e398a671d0401")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd21b08478979f7394612568e73da9493c79afdd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
