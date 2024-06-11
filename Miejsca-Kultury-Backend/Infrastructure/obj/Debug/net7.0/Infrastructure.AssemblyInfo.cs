c6bfbcf2d754c331e7df69c5067a2d421a08cba1
Jakub Jucha <jakubjucha800@gmail.com>
2024-06-11 21:57:51 +0200

    test

--- Miejsca-Kultury-Backend/Infrastructure/obj/Debug/net7.0/Infrastructure.AssemblyInfo.cs
+++ Miejsca-Kultury-Backend/Infrastructure/obj/Debug/net7.0/Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+462e21176087e043bdd40f82ed3789e3321c0fbc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2566f0d8be23a18f5613180bac19a12e40c43ee1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
