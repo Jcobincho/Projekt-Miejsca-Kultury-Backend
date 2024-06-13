1683681d270e4021385c5fde63c91f339adaca34
Enap1 <117779244+Enap1@users.noreply.github.com>
2024-06-13 22:20:08 +0200

    update Display post

--- Domain/obj/Debug/net7.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net7.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a3ed3e5dbee9257a0acc25bd3096011c7dd0505")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c8bf61ecd18fea9425eb81f05c5b3138bd8e5b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
