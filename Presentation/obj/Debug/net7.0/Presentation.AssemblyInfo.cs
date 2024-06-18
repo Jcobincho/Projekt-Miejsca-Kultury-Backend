3d8a760810784b3e886def030d53ba0e1a3f03e5
Jakub Jucha <jakubjucha800@gmail.com>
2024-06-18 19:26:07 +0200

    change links

--- Presentation/obj/Debug/net7.0/Presentation.AssemblyInfo.cs
+++ Presentation/obj/Debug/net7.0/Presentation.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3322fd3793d1a62ecb67175c9094c08770be4c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
