d0190503daadf9b3ecb802714adb53c27c3cd5e2
Jakub Jucha <jakubjucha800@gmail.com>
2024-06-19 22:30:42 +0200

    Final work

--- Api/obj/Debug/net7.0/Api.AssemblyInfo.cs
+++ Api/obj/Debug/net7.0/Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68d7869a6b928dd61ad89ec8543eef510e8fd7b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4752f20a184c4aba4fd6d023cfe941173dd2254f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
