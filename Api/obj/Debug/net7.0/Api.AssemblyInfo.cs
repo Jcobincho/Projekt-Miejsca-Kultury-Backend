1d71e5d5d1d17c5ef38046e9bc742813e90e15d1
Jakub Jucha <jakubjucha800@gmail.com>
2024-06-20 11:34:39 +0200

    change minio link

--- Api/obj/Debug/net7.0/Api.AssemblyInfo.cs
+++ Api/obj/Debug/net7.0/Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0190503daadf9b3ecb802714adb53c27c3cd5e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35448355fb2f0d1bbc3b313ee09f49d0d59ce5e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
